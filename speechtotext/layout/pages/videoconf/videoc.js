9249d368bd4fd2edabc9b28ecb4578b5212911a7
christine <chrisitne.brown@bhcc.edu>
2022-06-29 13:25:07 -0400

    add video conference

---
+++ speechtotext/layout/pages/videoconf/videoc.js
@@ -0,0 +1,13 @@
+
+
+{/* <script src="https://cdn.metered.ca/sdk/frame/1.4.2/sdk-frame.min.js"></script>
+<script>
+    var frame = new MeteredFrame(); 
+    frame.init({
+        roomURL: "spellme.metered.live/spellme",
+    }, document.getElementById("metered-frame"));
+</script>
+      
+        
+   
+<script src="//cdn.metered.ca/sdk/video/1.4.3/sdk.min.js"></script> */}
